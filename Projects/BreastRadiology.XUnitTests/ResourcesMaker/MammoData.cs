3ce8de36a45fac15dd034c76d6be4929b3aaf8f0
ROBIN\Kurt <KurtA@Gjoll.com>
2020-02-08 07:49:58 -0600

    .

--- Projects/BreastRadiology.XUnitTests/ResourcesMaker/MammoData.cs
+++ Projects/BreastRadiology.XUnitTests/ResourcesMaker/MammoData.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
+using System.Linq;
 using System.Text;
 using BreastRadiology.Shared;
 
@@ -40,6 +41,59 @@ namespace BreastRadiology.XUnitTests
             return false;
         }
 
-        public String UMLS => this.currentRow[this.BreastData.umlsCol].ToString();
+        public String UMLS => FormatUmls();
+
+
+        private String FormatUmls()
+        {
+            StringBuilder sb = new StringBuilder();
+            List<String> lines = this.currentRow[this.BreastData.umlsCol].ToString().Split('\n').ToList();
+            void CopyLines(Int32 count)
+            {
+                for (Int32 i = 0; i < count; i++)
+                    sb.AppendLine(lines[i]);
+            }
+
+            if (lines.Count > 0)
+                while ((lines.Count > 0) && String.IsNullOrWhiteSpace(lines[^1]))
+                    lines.RemoveAt(lines.Count - 1);
+            if (lines.Count == 0)
+                return "";
+
+            String lastLine = lines[^1];
+            if (lastLine.StartsWith("###") == false)
+            {
+                CopyLines(lines.Count);
+                return sb.ToString();
+            }
+
+            void AcrCitation(String name, String page)
+            {
+                sb.Append($"-- {name}");
+                if (String.IsNullOrEmpty(page) == false)
+                    sb.Append($"#{page}");
+                sb.AppendLine("");
+            }
+
+            CopyLines(lines.Count - 1);
+            lastLine = lastLine.Substring(3);
+            Int32 index = lastLine.IndexOf('#');
+            String citationType = lastLine.Substring(0, index);
+            String page = lastLine.Substring(index+1);
+            switch (citationType)
+            {
+                case "URL":
+                    String hRef = lastLine.Substring(index + 1);
+                    sb.Append($"-- {hRef}");
+                    break;
+                case "ACRUS":
+                    AcrCitation("Breast Imaging Reporting and Data System—Mammography, Fifth Edition", page);
+                    break;
+                case "ACRMG":
+                    AcrCitation("Breast Imaging Reporting and Data System—Ultrasound, Second Edition", page);
+                    break;
+            }
+            return sb.ToString();
+        }
     }
 }
